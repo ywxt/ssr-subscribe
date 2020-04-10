0e4f0dcb3a48c014ea01630901672c3ea4754981
ywxt <ywxtcwh@qq.com>
2020-04-10 15:57:09 +0800

    通过AsyncFileTest

--- src/test/kotlin/AsyncFileTest.kt
+++ src/test/kotlin/AsyncFileTest.kt
@@ -2,41 +2,56 @@ import kotlinx.coroutines.runBlocking
 import org.junit.Assert
 import org.junit.Test
 import ywxt.ssr.subscribe.async.file.AsyncFile
+import java.nio.file.StandardOpenOption
 
-class AsyncFileTest{
+class AsyncFileTest {
     @Test
-    fun readBytesTest() = runBlocking{
+    fun readBytesTest() = runBlocking {
         writeBytesTest()
-        val bytes = AsyncFile("src/test/kotlin/test.txt").use {
+        val bytes = AsyncFile("src/test/kotlin/test.txt", StandardOpenOption.READ).use {
             it.read()
         }
         Assert.assertNotNull(bytes)
         assert(bytes.isNotEmpty())
-        assert(String(bytes)=="Hello world")
+        assert(String(bytes) == "Hello world")
     }
 
     @Test
     fun writeBytesTest() = runBlocking {
         val bytes = "Hello world".toByteArray()
-        AsyncFile("src/test/kotlin/test.txt").use {
+        AsyncFile(
+            "src/test/kotlin/test.txt",
+            StandardOpenOption.WRITE,
+            StandardOpenOption.TRUNCATE_EXISTING,
+            StandardOpenOption.CREATE
+        ).use {
             it.write(bytes)
         }
     }
+
     @Test
     fun writeStringTest() = runBlocking {
         val string = """Hello worldHello worldHello worldHello worldHello worldHello worldHello
 | worldHello worldHello worldHello worldHello worldHello world""".trimMargin()
-        AsyncFile("src/test/kotlin/test.txt").use {
+        AsyncFile(
+            "src/test/kotlin/test.txt",
+            StandardOpenOption.WRITE,
+            StandardOpenOption.TRUNCATE_EXISTING,
+            StandardOpenOption.CREATE
+        ).use {
             it.writeString(string)
         }
     }
+
     @Test
-    fun readStringTest()= runBlocking {
+    fun readStringTest() = runBlocking {
         writeStringTest()
-        val string = AsyncFile("src/test/kotlin/test.txt").use {
+        val string = AsyncFile("src/test/kotlin/test.txt", StandardOpenOption.READ).use {
             it.readString()
         }
-        assert(string=="""Hello worldHello worldHello worldHello worldHello worldHello worldHello
-| worldHello worldHello worldHello worldHello worldHello world""".trimMargin())
+        assert(
+            string == """Hello worldHello worldHello worldHello worldHello worldHello worldHello
+| worldHello worldHello worldHello worldHello worldHello world""".trimMargin()
+        )
     }
 }
