5a9fbc1b9e60499b21c4c8a8f6b2d2dc8917bc99
ywxt <ywxtcwh@qq.com>
2020-04-25 21:49:17 +0800

    添加关于声明

--- src/main/kotlin/ywxt/ssr/subscribe/command/MainCommand.kt
+++ src/main/kotlin/ywxt/ssr/subscribe/command/MainCommand.kt
@@ -2,9 +2,13 @@ package ywxt.ssr.subscribe.command
 
 import com.github.ajalt.clikt.core.CliktCommand
 
-class MainCommand : CliktCommand(name = ""){
-    override fun run() {
-
-    }
+class MainCommand : CliktCommand(name = "ssr-sub", help = "SSR订阅工具", invokeWithoutSubcommand = true) {
+    override fun run() = if (currentContext.invokedSubcommand == null) {
+        println("实验竞赛月参赛作品")
+        println("名称: SSR订阅工具")
+        println("github: https://github.com/ywxt/ssr-subscribe")
+        println()
+        println(getFormattedHelp())
+    } else Unit
 
 }
